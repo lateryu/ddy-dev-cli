9c20080334c951975d70c6f4904af9f43630f00b
later <later@MacBook-Pro.local>
2021-05-07 18:41:43 +0800

    本地模板

---
+++ yff/public/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Table, Button } from 'antd';
+
+import './assets/css/index.scss'
+
+  class About extends React.Component {
+      render() {
+        return (<div>关于我们</div>);
+    }
+  }
+export default About;
